9fd1ef8d7b56ddf87d858aeaadcdfed25f089307
uxarjun9489 <arjunv9489@gmail.com>
2025-03-19 16:59:09 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,8 +7,14 @@ int main(){
     if(op == '+'){
         printf ("%d\n",a+b);
     }
-    else{
-        printf("0");
+    else if(op == '-'){
+        printf("%d\n",a-b);
+    }
+    else if(op == '*'){
+        printf("%d\n",a*b);
+    }
+    else (op == '/'){
+        printf("%d\n",a/b);
     }
     
 }
